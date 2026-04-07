dca7cce4aeb71af236c638dd1c68a7234684d235
vincentGagnon06 <vincegagnon2000@gmail.com>
2026-04-07 14:33:27 -0400

    Rebuild: Migrated project to Maven and JavaFX structure

---
+++ maven-project/src/main/java/com/vincentgagnon/Main.java
@@ -0,0 +1,34 @@
+package com.vincentgagnon;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.web.WebEngine;
+import javafx.scene.web.WebView;
+import javafx.stage.Stage;
+import java.net.URL;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage stage) {
+        WebView webView = new WebView();
+        WebEngine webEngine = webView.getEngine();
+
+        URL url = getClass().getResource("/web/index.html");
+
+        if (url != null) {
+            webEngine.load(url.toExternalForm());
+        } else {
+            System.err.println("CRITICAL: Could not find /web/index.html in resources.");
+        }
+
+        Scene scene = new Scene(webView, 1024, 768);
+        stage.setTitle("My Web-Based Application");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
